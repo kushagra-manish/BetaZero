a3caa12f5a05d165f21947a45583676964d52211
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-08-11 15:26:11 +0530

    Update.
    -> Undo move structure added
    -> Added pawn bitlist
    -> Now can read chess positions from FEN notation and display them. (Try it by replacing the string in BetaZero.cpp by your custom FEN position)
    -> 18/95

---
+++ BetaZeroCode/init.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include "defs.h"
+// this macro generates a 64 bit random integer rand() generates 15 bit integer
+#define RAND_64 (	(U64)rand() | \
+					(U64)rand() << 15 | \
+					(U64)rand() << 30 | \
+					(U64)rand() << 45 | \
+					((U64)rand() & 0xf) << 60    )
+					
+
+int Sq120ToSq64[BRD_SQ_NUM]; // array generating sq 64 from sq 120 
+int Sq64ToSq120[64]; // array generating sq 120 from sq 64
+
+U64 SetMask[64]; // array setting mask in a bit
+U64 ClearMask[64]; // array clearing a bit
+
+U64 PieceKeys[13][120]; // array for generating keys for pieces
+U64 SideKey; // array for generating key for which side is going on
+U64 CastleKeys[16]; // array for generating key for castlings 
+
+void InitHashKeys() {
+	// initializing hashes with random numbers
+	int index = 0;
+	int index2 = 0;
+	for(index = 0; index < 13; ++index) {
+		for(index2 = 0; index2 < 120; ++index2) {
+			PieceKeys[index][index2] = RAND_64;
+		}
+	}
+	SideKey = RAND_64;
+	for(index = 0; index < 16; ++index) {
+		CastleKeys[index] = RAND_64;
+	}
+
+}
+void InitBitMasks() {
+    // initializing bitmasks
+	int index = 0;
+	
+	for(index = 0; index < 64; index++) {
+		SetMask[index] = 0ULL;
+		ClearMask[index] = 0ULL;
+	}
+	
+	for(index = 0; index < 64; index++) {
+		SetMask[index] |= (1ULL << index);
+		ClearMask[index] = ~SetMask[index];
+	}
+}
+
+
+void InitSq120to64(){
+    //initialization
+    int index=0;
+    int file= FILE_A;
+    int rank= RANK_1;
+    int sq=A1;
+    int sq64=0;
+
+    //failsafes
+    for(index=0; index<BRD_SQ_NUM; ++index) Sq120ToSq64[index]=65;
+    for(index=0;index<64;++index) Sq64ToSq120[index]=120;
+    
+
+    //initializing the global array Sq120toSq64 and Sq64toSq120
+    for(rank = RANK_1; rank <= RANK_8; ++rank) {
+		for(file = FILE_A; file <= FILE_H; ++file) {
+			sq = FR2SQ(file,rank);
+			Sq64ToSq120[sq64] = sq;
+			Sq120ToSq64[sq] = sq64;
+			sq64++;
+		}
+	}
+}
+
+void AllInit(){
+    InitSq120to64();
+    InitBitMasks();
+    InitHashKeys();
+}
